c3ab03bba4725611dd07d6ad2fd5710ce71027b8
Rahil Mehta <65954612+Karma-Yogi-Rahil@users.noreply.github.com>
2024-02-15 17:30:45 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Finding_Album_Art
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
